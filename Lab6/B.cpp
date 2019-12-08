9939df8c0d4deb7acf77ec750fa0e400b0864dbf
Ilya K <ikuz_2002@mail.ru>
2019-12-08 20:49:39 +0300

    update README.md

---
+++ Lab6/B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream inputf("IOfiles/check.in");
+    ofstream outputf("IOfiles/check.out");
+    int n;
+    vector<int, pair<int, int>> nodes(n);
+    for (int i = 0; i < n; i++) {
+
+    }
+}
